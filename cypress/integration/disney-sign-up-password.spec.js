4c55628153e0d04b9db8c889aa15bb1f966cf32a
marko delgadillo <markoneseven@gmail.com>
2020-09-03 21:56:01 -0700

    Re-installed Cypress IO 5.0 within the local repo directory and moved out all of the examples to a different directory. Also completed the password spec.

--- cypress/integration/disney-sign-up-password.spec.js
+++ cypress/integration/disney-sign-up-password.spec.js
@@ -52,7 +52,7 @@ context("The Sign Up form modal", () => {
       .should("exist");
   });
 
-  it.only('verifies the password input field "The password is too short." error message', () => {
+  it('verifies the password input field "The password is too short." error message', () => {
     cy.getIframeBody()
       .find(".main .field-new-password input")
       .click()
@@ -66,7 +66,19 @@ context("The Sign Up form modal", () => {
       .should("exist");
   });
 
-  it('verifies the password input field "The password is too easily guessed." error message', () => {});
+  it('verifies the password input field "The password is too easily guessed." error message', () => {
+    cy.getIframeBody()
+      .find(".main .field-new-password input")
+      .click()
+      .type("123456")
+      .getIframeBody()
+      .find(".main .field-email input")
+      .click()
+      .getIframeBody()
+      .find(".main .field-new-password div.container .ng-scope")
+      .contains("The password is too easily guessed.")
+      .should("exist");
+  });
 
   it("verifies the password input field minimum requirements", () => {
     cy.getIframeBody()
@@ -78,12 +90,117 @@ context("The Sign Up form modal", () => {
       .getIframeBody()
       .find(".main .field-new-password")
       .click()
-      .should("have.class", "show-rules");
+      .should("have.class", "show-rules")
+      // validates the first rule
+      .getIframeBody()
+      .find(".main ul li.message-info")
+      .eq(0)
+      .contains("Enter at least 6 characters.")
+      .should("exist")
+      .should("be.visible")
+      // validates the second rule
+      .getIframeBody()
+      .find(".main ul li.message-info")
+      .eq(1)
+      .contains('Try a secret phrase, like: "dancing pizza hotel".')
+      .should("exist")
+      .should("be.visible")
+      // validates the third rule
+      .getIframeBody()
+      .find(".main ul li.message-info")
+      .eq(2)
+      .contains(
+        "Use letters together with spaces, numbers, or symbols (!@#$%^&*)."
+      )
+      .should("exist")
+      .should("be.visible");
   });
 
-  it('verifies using a single type of character only as "Weak"', () => {});
+  it('verifies using a single type of character only as "Weak"', () => {
+    cy.getIframeBody()
+      .find(".main .field-new-password input")
+      .click()
+      // using only alpha characters with minimum length
+      .type("abcdef")
+      .getIframeBody()
+      .find(".main .strength-meter .state-weak")
+      .contains("Weak")
+      .should("exist")
+      .should("be.visible")
+      .getIframeBody()
+      .find(".main .strength-meter")
+      .should("have.attr", "score", "2")
+      .getIframeBody()
+      .find(".main .field-new-password input")
+      .clear()
 
-  it('verifies using alpha/numeric characters as "Ok"', () => {});
+      .getIframeBody()
+      .find(".main .field-new-password input")
+      .click()
+      // using numbers only with minimum length
+      .type("123456")
+      .getIframeBody()
+      .find(".main .strength-meter .state-weak")
+      .contains("Weak")
+      .should("exist")
+      .should("be.visible")
+      .getIframeBody()
+      .find(".main .strength-meter")
+      .should("have.attr", "score", "2");
+  });
 
-  it('verifies using two or more types of characters as with a minimum of 12 characters as "Strong"', () => {});
+  it('verifies using alpha/numeric characters as "OK"', () => {
+    cy.getIframeBody()
+      .find(".main .field-new-password input")
+      .click()
+      // using alpha/numeric and special characters at minimum length
+      .type("1!a2@b")
+      .getIframeBody()
+      .find(".main .strength-meter .state-ok")
+      .contains("OK")
+      .should("exist")
+      .should("be.visible")
+      .getIframeBody()
+      .find(".main .strength-meter")
+      .should("have.attr", "score", "3");
+  });
+
+  it('verifies using two or more types of characters with a minimum of 12 characters as "Strong"', () => {
+    cy.getIframeBody()
+      .find(".main .field-new-password input")
+      .click()
+      // using alpha/numeric and special characters at minimum length
+      .type("1!a2@b3#c4$d5%e")
+      .getIframeBody()
+      .find(".main .strength-meter .state-strong")
+      .contains("Strong")
+      .should("exist")
+      .should("be.visible")
+      .getIframeBody()
+      .find(".main .strength-meter")
+      .should("have.attr", "score", "6");
+  });
+
+  it('verifies using an invalid password displays "invalid"', () => {
+    cy.getIframeBody()
+      .find(".main .field-new-password input")
+      .click()
+      // using only alpha characters with less than the minimum length
+      .type("abcdef")
+      .getIframeBody()
+      .find(".main .field-email input")
+      .click()
+      .getIframeBody()
+      .find(".main .field-new-password input")
+      .click()
+      .getIframeBody()
+      .find(".main .strength-meter")
+      .should("have.class", "invalid")
+      .should("be.visible")
+      .should("have.attr", "score", "-1")
+      .getIframeBody()
+      .find(".main .strength-meter .state-invalid")
+      .contains("Invalid")
+      .should("exist");
+  });
 });
