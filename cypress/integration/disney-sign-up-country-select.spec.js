3fc7d6911a0414983557ab5b07ca240e89ec7ca9
marko delgadillo <markoneseven@gmail.com>
2020-09-24 16:14:16 -0700

    Created the spec for the country select drop down and added the first four test descriptions.

---
+++ cypress/integration/disney-sign-up-country-select.spec.js
@@ -0,0 +1,11 @@
+context("The Sign up modal Country/Region drop down", () => {
+  beforeEach(() => {
+    cy.visit("");
+    cy.clickSignUp();
+  });
+
+  it("is displayed.", () => {});
+  it('has the value "Country/Region."', () => {});
+  it("opens the drop down when clicked.", () => {});
+  it("selects the United States.", () => {});
+});
